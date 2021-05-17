8f225082d2d9ddbaf4d741ffce7df1e9baf74269
nieshihao <913653365@qq.com>
2021-05-17 21:21:04 +0800

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int test01()
+{
+    int a = 10, b = 20;
+    cout << "Before swap a = " << a << ", b = " << b << endl;
+    swap(a, b);
+    cout << "After swap a = " << a << ", b = " << b << endl;
+}
+
 int main() {
     cout << "my first project!" << endl;
+    test01();
     return 0;
 }
